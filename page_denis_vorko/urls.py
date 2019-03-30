061de60fee797224fa2b7bf3d272bc508117c466
ihoryarosh <hedgegog1@gmail.com>
2019-03-30 19:18:28 +0200

    added page about

--- page_denis_vorko/urls.py
+++ page_denis_vorko/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('', views.index, name='index_DV'),
+    url(r'about/.+', views.about, name='about_job')
 ]
--- page_denis_vorko/views.py
+++ page_denis_vorko/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Job
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     jobs = Job.objects.all()
     return render(request, 'page_denis_vorko/index.html', context={'jobs':jobs})
+
+
+def about(request):
+    job = Job.objects.get(title=request.path.split('/')[-1])
+    return render(request, 'page_denis_vorko/about.html', context={'job':job})
+
+    
